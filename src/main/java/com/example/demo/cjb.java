57d195952beaedcecfe98b462edf8fbc51064be5
mzj <25425123@qq.com>
2019-11-02 12:12:18 +0800

    V1.01

---
+++ src/main/java/com/example/demo/cjb.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import jdk.nashorn.internal.objects.annotations.Function;
+
+import javax.xml.ws.ServiceMode;
+import javax.xml.ws.soap.Addressing;
+import java.beans.Transient;
+
+@Addressing
+
+@ServiceMode
+public class cjb {
+    public static void main(String[] args) {
+
+    }
+}
